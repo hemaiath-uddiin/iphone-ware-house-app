4c91ee0c29d1fb9bfe10a93f6abe62cc21ef9662
hemaith-uddin <hemaithisifat@gmail.com>
2022-05-13 21:01:09 +0600

    service section add

--- src/Component/FeaturesItem/FeaturesItem.js
+++ src/Component/FeaturesItem/FeaturesItem.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Services = () => {
+    const [item, setItem] = useState([]);
+    useEffect(() => {
+        fetch('item.json')
+            .then(res => res.json())
+            .then(data => setItem(data))
+
+    }, [])
     return (
-        <div>
+        <div className='container'>
             <h2 className='text-center'> feature Items</h2>
+            <div className="row">
+                {
+                    item.map((items) => {
+                        const { name, img, des, price, quantity } = items
+                        return (
+                            <div className="col-lg-4">
+                                <div class="card" >
+                                    <img src={img} class="card-img-top" alt="..." />
+                                    <div class="card-body">
+                                        <h5 class="card-title"> {name} </h5>
+                                        <p class="card-text"> {des}</p>
+                                        <p class="card-text"> {price}</p>
+                                        <p class="card-text"> {quantity}</p>
+                                        <a href="/" class="btn btn-primary"> updated</a>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+
+
+            </div>
+
         </div>
     );
 };
--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
-import Navbar from '../Navbar/Navbar';
+
 import Services from '../FeaturesItem/FeaturesItem';
 
 const Home = () => {
