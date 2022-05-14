10882e61e102b33af1d2bfeb828bc3d6ef61acb2
hemaith-uddin <hemaithisifat@gmail.com>
2022-05-14 20:46:29 +0600

    google implement added

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,12 +1,19 @@
+import { signOut } from 'firebase/auth';
 import React from 'react'; 
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
+import auth from '../../fire.init.auths';
 
 
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
 
 const Navbar = () => {
-    return (
+   const [user] = useAuthState(auth) ; 
+    const signOUt=()=>{ 
+      signOut(auth)
+    }
+  return (
         <div>
        <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
@@ -23,7 +30,13 @@ const Navbar = () => {
           <Link className="nav-link active" aria-current="page" to="/item">Feature</Link>
         </li>
         <li className="nav-item">
+          {  
+          user? 
+           <button onClick={signOUt}> sigout</button> 
+          :
           <Link className="nav-link" to="/loging">Loging</Link>
+          
+          } 
         </li>
         
        
