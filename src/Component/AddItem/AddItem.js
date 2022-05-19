8c9e09b8759752897873111ce5b0f3b1ec602ba9
hemaith-uddin <hemaithisifat@gmail.com>
2022-05-20 00:17:22 +0600

    increass and decreass added

--- src/Component/AddItem/AddItem.js
+++ src/Component/AddItem/AddItem.js
@@ -1,9 +1,37 @@
-import React from 'react';
 
+import React from 'react';
+import { useForm } from "react-hook-form";
 const AddItem = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => {
+       
+  
+        console.log(data);
+        const url =`http://localhost:5000/item`
+        fetch(url, {
+            method: 'POST',
+            headers:{'content-type': 'application/json'},
+            body: JSON.stringify(data)
+           
+        })
+        .then(res =>res.json())
+        .then(result =>{
+            alert( "added", result)
+        })
+    }
     return (
         <div>
             <h2>this is add item </h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input placeholder='name' {...register("Name", { required: true, maxLength: 20 })} />
+
+                <input placeholder='description' {...register("description",)} />
+                <input placeholder='photo url' type="text" {...register("img",)} />
+                <input placeholder='price' type="number" {...register("price",)} />
+                <input placeholder='quantity' type="number" {...register("quantity",)} />
+
+                <input type="submit" />
+            </form>
         </div>
     );
 };
--- src/Component/Updated/Update.js
+++ src/Component/Updated/Update.js
@@ -3,40 +3,72 @@ import { useState, useEffect } from 'react'
 import { useParams, } from 'react-router-dom';
 
 const Update = ({ children }) => {
-    const { id } = useParams()
+    const { id } = useParams();
     const [setupdateDtls, setUpdateDtls] = useState({}) ;
-    const [update,setUpdate] = useState() ; 
+    const [update,setUpdate] = useState('') ; 
       const updateQuantity =(e)=>{ 
           setUpdate(e.target.value)
       }
-    
-      
-    useEffect(() => {
-        const url = `http://localhost:5000/item/${id}`;
+      const loadData =()=>{ 
+        const url =`http://localhost:5000/item/${id}`;
         fetch(url)
             .then(res => res.json())
             .then(addQuantity => setUpdateDtls(addQuantity))
-    }, [])
+      }
+     
+    useEffect(() => {
+       loadData() 
+     
+    },[])  
+
+       const deleverd=()=>{ 
+           const newQuantity = setupdateDtls.quantity-1 ;
+           const url =`http://localhost:5000/item/${id}`;
+           fetch(url, {
+                        method:"PUT",
+                        headers:{'content-type': 'application/json'},
+                        body: JSON.stringify({
+                            newQuantity
+                        }),
+                       
+                    }) 
+                    .then(res=>res.json()) 
+                        .then(json=>{ 
+                            console.log("succes",json);  
+                            loadData()
+                            
+                        })
+           
+       }
     const updateaddQuantity =(e)=>{ 
         e.preventDefault() ;
         let newQuantity = update ; 
          if(newQuantity> 0){ 
     const addQuantity = parseInt(setupdateDtls.quantity)+parseInt(newQuantity)
      console.log(addQuantity);
-    // addQuantity send to the server  
-    fetch(`http://localhost:5000/item/${id}`, {
-        method: 'PUT', // or 'PUT'
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(
-            addQuantity
-        ),
-      }) 
-      .then(res => res.json()) 
-      .then(json => {
-        console.log('Success:', json);
-      })
+     let data = {
+        newQuantity: addQuantity
+     } 
+
+    // addQuantity send to the server   
+         try {
+            fetch(`http://localhost:5000/item/${id}`, {
+                method:"PUT",
+                headers:{'content-type': 'application/json'},
+                body: JSON.stringify(data)
+               
+            })
+            .then(res=>res.json()) 
+            .then(json=>{ 
+                console.log("succes",json); 
+                loadData()
+            })
+              
+         } catch (error) {
+             console.log(error,"error is here");
+         }
+ 
+      
     
      
 
@@ -56,8 +88,8 @@ const Update = ({ children }) => {
                                 <p className="card-text"> quantity: {setupdateDtls.quantity} </p>
                                 <p className="card-text"> Price: {setupdateDtls.price}  </p>
                                 <p className="card-text"> Description: {setupdateDtls.des}  </p>
-                                <button className='btn btn-danger'> Deleverd</button>
-                                <button className='btn btn-primary'> Manage Inventories </button>
+                                <button onClick={()=>{deleverd()}}  className='btn btn-danger'> Deleverd</button>
+                                <button  className='btn btn-primary'> Manage Inventories </button>
                             </div>
                         </div>
 
